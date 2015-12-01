68cfa930aad4cefafd25f7b18bb5d419fd112d44
reinhardtcgr <reinhardt.cgr@gmail.com>
2015-12-01 22:32:56 +0000

    Able to implement the list all contacts function to the button

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,16 +1,26 @@
 $(document).ready(function() {
   alert('everything has loaded');
 
-  // function listContacts() {
-  //   return $.ajax({
-  //     url: '/list',
-  //     method: 'GET',
-  //     data: {  }
-  //   });
-  // }
+  function listContacts() {
+    return $.ajax({
+      url: '/list',
+      method: 'GET',
+      data: {  }
+    });
+  }
 
   $('#list').on('click', function getAll() {
-    $('#friends').append('button works');
+    var p = $('<p>').text('button clicked');
+    var friend_box = $('#friends');
+    friend_box.append(p);
+
+    listContacts().done(function addToPage(data) {
+      for (var i = 0; i < data.length; i++) {
+        var box = $('<div>').text(data[i].email);
+        friend_box.append(box);
+      }
+    });
+
   });
 
 });
