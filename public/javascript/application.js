6d14eec44f172349b07a6352f29bf04d3b8273af
reinhardtcgr <reinhardt.cgr@gmail.com>
2015-12-02 19:31:24 +0000

    Separated the major function for listing all contacts, into a handler

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -10,32 +10,29 @@ $(document).ready(function() {
     },
     newContact: function (contact) {
       return $.ajax({
-        url: '',
+        url: '/contacts',
         method: 'POST',
         dataType: 'json',
         data: contact
+      });
+    },
+    getContacts: function () {
+      handlers.listContacts().done(function addAllToPage(contacts) {
+        $.each(contacts, function (index, contact) {
+          var row = $('<tr>').appendTo(handlers.container);
+          $('<td>').text(contact.firstname).appendTo(row);
+          $('<td>').text(contact.lastname).appendTo(row);
+          $('<td>').text(contact.email).appendTo(row);
+          // // var contactNumbers = $('<td>').text('numbers: ' + data[i].numbers);
+          // may have to do a search within a search
+          // list within a list for the numbers
+        });
       });
     }
   };
 
 
-  $('#list').on('click', function getAll() {
-    // var container = $('#friends');
-
-    handlers.listContacts().done(function addAllToPage(data) {
-
-      for (var i = 0; i < data.length; i++) {
-        var row = $('<tr>').appendTo(handlers.container);
-        $('<td>').text(data[i].firstname).appendTo(row);
-        $('<td>').text(data[i].lastname).appendTo(row);
-        $('<td>').text(data[i].email).appendTo(row);
-        // var contactNumbers = $('<td>').text('numbers: ' + data[i].numbers);
-        // may have to do a search within a search
-        // list within a list for the numbers
-      }
-    });
-
-  });
+  $('#list').on('click', handlers.getContacts);
 
 
   $('#add').on('click', function addContact() {
